a57b7d10173566125391b7815611798f1dae8f38
Wojciech Gajda <wggajda@gmail.com>
2023-04-01 21:34:34 +0200

    Same Qt changes

--- GUI/__main__.py
+++ GUI/__main__.py
@@ -1,9 +1,9 @@
 import sys
-from PySide6.QtCore import Qt
-from PySide6.QtWidgets import QApplication, QLabel
-                                                     
-if __name__ == "__main__":
-    app = QApplication(sys.argv)
-    label = QLabel("Hello World", alignment=Qt.AlignCenter)
-    label.show()
-    sys.exit(app.exec_())
+from PySide6 import QtCore, QtGui, QtWidgets
+from PySide6.QtUiTools import QUiLoader
+
+loader = QUiLoader()
+app = QtWidgets.QApplication(sys.argv)
+window = loader.load("forms/main.ui", None)
+window.show()
+app.exec_()
