468ee96ec007ecc6a2b24a18d6264df197f6704a
Eri <eryk.jamroz@gmail.com>
2023-04-27 11:10:27 +0200

    cleaned

--- GUI/__main__.py
+++
@@ -1,9 +0,0 @@
-import sys
-from PySide6 import QtCore, QtGui, QtWidgets
-from PySide6.QtUiTools import QUiLoader
-
-loader = QUiLoader()
-app = QtWidgets.QApplication(sys.argv)
-window = loader.load("forms/main.ui", None)
-window.show()
-app.exec_()
--- install.md
+++
@@ -1,6 +0,0 @@
-git submodule init\
-git submodule update --init --recursive\
-conda create -n FANUC_CNC python=3.9\
-conda activate FANUC_CNC\
-pip install pyside6\
-python GUI\
